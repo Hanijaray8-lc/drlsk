ac924e9c6ac8db34f6e8e21f52fe76ea19dad89f
Hanijaray8-lc <lcproduct566@gmail.com>
2026-05-28 13:11:25 +0530

    Use system Chromium browser for Render deployment

--- whatsapp_bot_render.js
+++ whatsapp_bot_render.js
@@ -19,7 +19,8 @@ function initClient() {
         }),
         puppeteer: {
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser'
         }
     });
 
