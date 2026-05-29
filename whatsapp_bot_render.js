37abdac61df9f30633d7b4a9a94b5e8c8bbd8892
Hani☠️ <hanija.lc240073@gmail.com>
2026-05-29 16:17:23 +0530

    Update whatsapp_bot_render.js

--- whatsapp_bot_render.js
+++ whatsapp_bot_render.js
@@ -15,9 +15,11 @@ const client = new Client({
         dataPath: "./.wwebjs_auth"
     }),
 
-    webVersionCache: {
-        type: "remote"
-    },
+   webVersionCache: {
+    type: "remote",
+    remotePath:
+        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html"
+},
 
     puppeteer: {
         headless: true,
